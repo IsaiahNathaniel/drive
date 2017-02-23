aba823ba837c6ea32da1f44b28874aa8ac639e5e
Isaiah <isaiah712.minniear@gmail.com>
2017-02-23 13:01:58 -0500

    Movement Changes

--- main.js
+++ main.js
@@ -41,12 +41,12 @@ var mainState = {
          this.player_one = this.game.add.sprite(400, 550, 'player');
          game.physics.arcade.enable(this.player_one)
          this.player_one.anchor.setTo(0.5, 0.5);
-         this.player_one.collideWorldBounds=true;
+         this.player_one.collideWorldBounds = true;
         
-         this.player_two = this.game.add.sprite(400, 550, 'player');
+         this.player_two = this.game.add.sprite(300, 350, 'player');
          game.physics.arcade.enable(this.player_two)
          this.player_two.anchor.setTo(0.5, 0.5);
-         this.player_two.collideWorldBounds=true;
+         this.player_two.collideWorldBounds = true;
         // player group init --- disabled as players may be handled a different way
         /*
         this.players = game.add.group();  // initialize player group
@@ -98,7 +98,7 @@ var mainState = {
         }
         
         // player_two inputs
-        this.player_two.body.velocity.setTo(0,0);
+        this.player_two.body.velocity.setTo(this.player_two.body.velocity.x/3,this.player_two.body.velocity.y/3);
         if (this.movement.left.isDown){
             this.player_two.body.velocity.x = -200;
         }
