75b4f419834ff4382c025b6c2836fce862e047eb
Isaiah <isaiah712.minniear@gmail.com>
2017-02-23 12:18:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -46,14 +46,21 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.movement = game.input.keyboard.createCursorKeys();
         
-        this.newPlayer = "player_one"
         
-        this.createPlayers(newPlayer);
         
         // score init
         
         
+        // player inits
+         this.player_one = this.game.add.sprite(400, 550, 'player');
+         game.physics.arcade.enable(this.player_one)
+         this.player_one.anchor.setTo(0.5, 0.5);
+         this.player_one.collideWorldBounds=true;
         
+         this.player_two = this.game.add.sprite(400, 550, 'player');
+         game.physics.arcade.enable(this.player_two)
+         this.player_two.anchor.setTo(0.5, 0.5);
+         this.player_two.collideWorldBounds=true;
         // player group init --- disabled as players may be handled a different way
         /*
         this.players = game.add.group();  // initialize player group
@@ -84,6 +91,8 @@ var mainState = {
         game.state.start('main')
     },
     
+    
+    // this function may not be used
     createPlayers: function(playerName) {
          this.playerName = this.game.add.sprite(400, 550, 'player');
          game.physics.arcade.enable(this.playerName)
