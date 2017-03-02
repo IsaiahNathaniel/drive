176c8002d701ce8e49ef8680add00ce21debb500
Isaiah <isaiah712.minniear@gmail.com>
2017-03-02 13:33:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ var mainState = {
         if (this.movement.up.isDown) {
             this.player_one.body.velocity.x = Math.cos(70);
             this.player_one.body.velocity.y = Math.sin(70);
+            console.log(Math.cos(70));
         }
         if (this.movement.down.isDown) {
             this.player_one.body.velocity.y = this.player_one.speed;
