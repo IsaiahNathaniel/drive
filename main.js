85591bde3b4cabf3d3086126a73fda7ddc3b06c0
Isaiah <isaiah712.minniear@gmail.com>
2017-03-15 13:44:26 -0400

    Removed sprite diappearing

--- main.js
+++ main.js
@@ -83,7 +83,9 @@ var mainState = {
             this.player_one.speedLimit = 10000000;
         }
         if (this.movement.left.isDown){
-           this.player_one.rotation -= ((10 * Math.sqrt(this.player_one.leftTurnMod * (1/this.player_one.speed))) * 0.0174533);    // it's easier to visualize rotation amount in degrees -> radians
+           if (this.player_one.turnLeftMod > 0 && this.player_one.speed > 0) {
+               this.player_one.rotation -= ((10 * Math.sqrt(this.player_one.leftTurnMod * (1/this.player_one.speed))) * 0.0174533);    // it's easier to visualize rotation amount in degrees -> radians
+           }
            if (this.player_one.leftTurnMod < 3.0) {
                this.player_one.leftTurnMod += 0.06;
            }
