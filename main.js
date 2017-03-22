318ffa0dbe3dca5a2fbe611ff1a5059b301d07ef
Isaiah <isaiah712.minniear@gmail.com>
2017-03-22 13:44:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -58,9 +58,6 @@ var mainState = {
         walls.setAll("anchor.y", 0.5);
         walls.setAll("scale.x", 0.5);
         walls.setAll("scale.y", 0.5);
-        walls.setAll("angle", 0.5);
-        walls.setAll("width", 3);
-        
         
     },
     
