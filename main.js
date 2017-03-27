771c4617bcfcecfebb2fd6aefff528531676a0b6
Isaiah <isaiah712.minniear@gmail.com>
2017-03-27 09:51:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ var mainState = {
 		this.playerInputCheck();
 		this.checkWallPlace();
 		
-		// game.physics.arcade.collide(this.player_one, this.walls, this.collisionDetected, null, this);
+		game.physics.arcade.collide(this.player_one, this.walls, this.collisionDetected, null, this);
 		
     },
     
